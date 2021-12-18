e2d45c0090958914319875efd3f5315e95433cc4
BlueWhaleMain <bluewhalemain@outlook.com>
2021-12-18 15:39:51 +0800

    发布CipherManagerConsole 1.0.0.1

--- build-console.bat
+++ build-console.bat
@@ -1,6 +1,6 @@
 @rem Copyright by BlueWhale. All Rights Reserved.
 @rem Encoding:GBK
 @echo off
-call builds.bat CipherManagerConsole 1.0.0.0
+call builds.bat CipherManagerConsole 1.0.0.1
 if %errorlevel% GTR 0 echo ¹¹½¨Ê§°Ü£¡
 pause
