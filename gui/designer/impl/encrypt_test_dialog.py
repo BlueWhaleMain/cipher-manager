faac34b751325117c309f05514dafe88281dc0cd
BlueWhaleMain <bluewhalemain@outlook.com>
2023-02-09 21:51:58 +0800

    完善了加密测试

--- gui/designer/impl/encrypt_test_dialog.py
+++ gui/designer/impl/encrypt_test_dialog.py
@@ -1,4 +1,5 @@
 import threading
+import typing
 
 import OpenSSL.crypto
 from PyQt5 import QtWidgets, QtGui
@@ -6,9 +7,14 @@ from PyQt5 import QtWidgets, QtGui
 from gui.designer.encrypt_test_dialog import Ui_EncryptTestDialog
 from gui.widgets.item.readonly import ReadOnlyItem
 
-ENCRYPT_ALGORITHM = ['DES']
-HASH_ALGORITHM = ['EDE3']
-MODE = ['ECB', 'CBC', 'CFB', 'OFB']
+
+def test_tuple() -> typing.Generator[tuple[str, str, str], None, None]:
+    yield 'DES', 'EDE3', 'CBC'
+    sizes = ['128', '192', '256']
+    aes_modes = ['ECB', 'CBC', 'CFB', 'OFB']
+    for s in sizes:
+        for m in aes_modes:
+            yield 'AES', s, m
 
 
 class EncryptTestDialog(QtWidgets.QDialog, Ui_EncryptTestDialog):
@@ -27,20 +33,18 @@ class EncryptTestDialog(QtWidgets.QDialog, Ui_EncryptTestDialog):
         green_color = QtGui.QColor('green')
         pk = OpenSSL.crypto.PKey()
         pk.generate_key(OpenSSL.crypto.TYPE_RSA, 1024)
-        for enc in ENCRYPT_ALGORITHM:
-            for has in HASH_ALGORITHM:
-                for mod in MODE:
-                    name = f'{enc}-{has}-{mod}'
-                    row = [ReadOnlyItem(name), ReadOnlyItem(enc), ReadOnlyItem(mod)]
-                    self.model.appendRow(row)
-                    try:
-                        OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, pk, name, b'123')
-                        for r in row:
-                            r.setForeground(green_color)
-                    except Exception as e:
-                        for r in row:
-                            r.setForeground(red_color)
-                            r.setToolTip(str(e))
+        for enc, has, mod in test_tuple():
+            name = f'{enc}-{has}-{mod}'
+            row = [ReadOnlyItem(name), ReadOnlyItem(enc), ReadOnlyItem(mod)]
+            self.model.appendRow(row)
+            try:
+                OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, pk, name, b'123')
+                for r in row:
+                    r.setForeground(green_color)
+            except Exception as e:
+                for r in row:
+                    r.setForeground(red_color)
+                    r.setToolTip(str(e))
         self._task = None
 
     def run(self):
