48a5cd1a60d2dc411e3deff8aa8a2eb06247dc39
BlueWhaleMain <bluewhalemain@outlook.com>
2023-03-25 15:47:46 +0800

    :bookmark: CipherManagerGUI 版本号更新至 1.0.2.0

--- build-gui.bat
+++ build-gui.bat
@@ -1,6 +1,6 @@
 @rem Copyright by BlueWhale. All Rights Reserved.
 @rem Encoding:GBK
 @echo off
-call builds.bat CipherManagerGUI 1.0.1.1
+call builds.bat CipherManagerGUI 1.0.2.0
 if %errorlevel% GTR 0 echo ¹¹½¨Ê§°Ü£¡
 pause
