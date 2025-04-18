14b79358c5a3721c93597c68729efa0a4fdfdf17
BlueWhaleMain <bluewhalemain@outlook.com>
2025-04-18 12:56:14 +0800

    :fire: 删除已过时的控制台版本

--- build-console.bat
+++
@@ -1,6 +0,0 @@
-@rem Copyright by BlueWhale. All Rights Reserved.
-@rem Encoding:GBK
-@echo off
-call builds.bat CipherManagerConsole 1.1.0.0 future
-if %errorlevel% GTR 0 echo ¹¹½¨Ê§°Ü£¡
-pause
--- console_main.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from console.main import main
-
-
-if __name__ == '__main__':
-    main()
